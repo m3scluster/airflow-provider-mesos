80b0ea328a7ba96cc3b56113b45cb4e814e2b01a
Andreas Peters <ap@aventer.biz>
2021-10-21 18:21:55 +0200

    Update requirement in the setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     license="Apache License 2.0",
     packages=find_packages(),
     install_requires=["apache-airflow>=2.0"],
-    setup_requires=["avmesos"],
+    setup_requires=["avmesos", "waitress"],
     author="Andreas Peters",
     author_email="support@aventer.biz",
     url="https://www.aventer.biz/",
