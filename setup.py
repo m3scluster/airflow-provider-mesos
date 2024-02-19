65ecaec7586d86bbef5e2971fd641a145c07b235
Andreas Peters <ap@aventer.biz>
2024-02-19 17:10:00 +0100

    UPDATE: v0.2.7

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except UnicodeDecodeError:
 
 setup(
     name="avmesos_airflow_provider",
-    version="0.2.6",
+    version="0.2.7",
     description="Apache Mesos Provider",
     long_description=README,
     long_description_content_type="text/markdown",
